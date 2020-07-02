48411903aa5b978860ac792a4c6c1f08004a6b40
Ajay Yadav <ajayy101@gmail.com>
2020-07-02 13:20:39 -0400

    added input card for adding trasnaction

--- flutter/personal_expenses_app/lib/main.dart
+++ flutter/personal_expenses_app/lib/main.dart
@@ -1,14 +1,138 @@
 import 'package:flutter/material.dart';
+import './transaction.dart';
+import 'package:intl/intl.dart';
 
 void main() => runApp(HomePage());
 
 class HomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: '1',
+      title: 'Walmart',
+      amount: 10.12,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: '2',
+      title: 'Lotte',
+      amount: 15.12,
+      date: DateTime.now(),
+    ),
+  ];
+
+  // String titleInput;
+  // String amountInput;
+
+  final titleControler = TextEditingController();
+  final amountControler = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("Personal Expenses App"),
+        ),
+        body: Column(
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.blue,
+                child: Text(
+                  'Chart',
+                  style: TextStyle(color: Colors.white),
+                ),
+                elevation: 5,
+              ),
+            ),
+            Card(
+              child: Container(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Title'),
+                      controller: titleControler,
+                      // onChanged: (value) {
+                      //   titleInput = value;
+                      // },
+                    ),
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Amount'),
+                      keyboardType:
+                          TextInputType.numberWithOptions(decimal: true),
+                      controller: amountControler,
+                      // onChanged: (value) {
+                      //   amountInput = value;
+                      // },
+                    ),
+                    FlatButton(
+                      child: Text('Add Transaction'),
+                      textColor: Colors.purple,
+                      onPressed: () {
+                        print(titleControler.text);
+                        print(amountControler.text);
+                      },
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Column(
+              children: transactions
+                  .map((txn) => Card(
+                        elevation: 3,
+                        child: Row(
+                          children: <Widget>[
+                            Container(
+                              padding: EdgeInsets.all(10),
+                              margin: EdgeInsets.symmetric(
+                                vertical: 10,
+                                horizontal: 15,
+                              ),
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                  width: 2,
+                                  color: Colors.purple,
+                                ),
+                              ),
+                              child: Text(
+                                '\$${txn.amount}',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20,
+                                  color: Colors.purple,
+                                ),
+                              ),
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  txn.title,
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Text(
+                                  DateFormat('MMM dd, yyyy').format(txn.date),
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ))
+                  .toList(),
+            )
+          ],
         ),
       ),
     );
