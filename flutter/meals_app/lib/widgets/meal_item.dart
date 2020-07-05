72d9a5f76727fbeaf4ff4886b956dfd663fa6b7b
Ajay Yadav <ajayy101@gmail.com>
2020-07-04 22:12:08 -0400

    added meal detail item

--- flutter/meals_app/lib/widgets/meal_item.dart
+++ flutter/meals_app/lib/widgets/meal_item.dart
@@ -16,6 +16,32 @@ class MealItem extends StatelessWidget {
     this.affordability,
   });
 
+  String get complexityText {
+    switch (complexity) {
+      case Complexity.Hard:
+        return 'Hard';
+      case Complexity.Challenging:
+        return 'Challenging';
+      case Complexity.Simple:
+        return 'Simple';
+      default:
+        return 'Unknown';
+    }
+  }
+
+  String get affordabilityText {
+    switch (affordability) {
+      case Affordability.Affordable:
+        return 'Affordable';
+      case Affordability.Luxurious:
+        return 'Luxurious';
+      case Affordability.Pricey:
+        return 'Pricey';
+      default:
+        return 'Unknown';
+    }
+  }
+
   void selectMeal() {}
 
   @override
@@ -33,18 +59,78 @@ class MealItem extends StatelessWidget {
             Stack(
               children: <Widget>[
                 ClipRRect(
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(15),
-                      topRight: Radius.circular(15),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(15),
+                    topRight: Radius.circular(15),
+                  ),
+                  child: Image.network(
+                    imageUrl,
+                    height: 250,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Positioned(
+                  bottom: 10,
+                  right: 0,
+                  child: Container(
+                    width: 300,
+                    color: Colors.black54,
+                    padding: EdgeInsets.symmetric(
+                      vertical: 5,
+                      horizontal: 20,
                     ),
-                    child: Image.network(
-                      imageUrl,
-                      height: 250,
-                      width: double.infinity,
-                      fit: BoxFit.cover,
-                    )),
+                    child: Text(
+                      title,
+                      style: TextStyle(
+                        fontSize: 26,
+                        color: Colors.white,
+                      ),
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
+                )
               ],
             ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.schedule,
+                        color: Theme.of(context).primaryColor,
+                      ),
+                      SizedBox(width: 6),
+                      Text('$duration min')
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.work,
+                        color: Theme.of(context).primaryColor,
+                      ),
+                      SizedBox(width: 6),
+                      Text(complexityText)
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.work,
+                        color: Theme.of(context).primaryColor,
+                      ),
+                      SizedBox(width: 6),
+                      Text(affordabilityText)
+                    ],
+                  ),
+                ],
+              ),
+            )
           ],
         ),
       ),
