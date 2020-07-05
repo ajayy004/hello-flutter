597ca49611ff0eacfc00b72e59a445d63afeab2d
Ajay Yadav <ajayy101@gmail.com>
2020-07-05 19:10:27 -0400

    using consumer to just update the specific widget

--- flutter/shop_app/lib/widgets/product_item.dart
+++ flutter/shop_app/lib/widgets/product_item.dart
@@ -7,7 +7,7 @@ import '../screens/product_detail.dart';
 class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context);
+    final product = Provider.of<Product>(context, listen: false);
 
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
@@ -30,13 +30,22 @@ class ProductItem extends StatelessWidget {
             product.title,
             textAlign: TextAlign.center,
           ),
-          leading: IconButton(
-            icon: Icon(
-              product.isFavorite ? Icons.favorite : Icons.favorite_border,
+          leading: Consumer<Product>(
+            builder: (ctx, product, child) => IconButton(
+              icon: Icon(
+                product.isFavorite ? Icons.favorite : Icons.favorite_border,
+              ),
+              color: Theme.of(context).accentColor,
+              onPressed: product.toggleFavoriteStatus,
             ),
-            color: Theme.of(context).accentColor,
-            onPressed: product.toggleFavoriteStatus,
           ),
+          // leading: IconButton(
+          //   icon: Icon(
+          //     product.isFavorite ? Icons.favorite : Icons.favorite_border,
+          //   ),
+          //   color: Theme.of(context).accentColor,
+          //   onPressed: product.toggleFavoriteStatus,
+          // ),
           trailing: IconButton(
             icon: Icon(Icons.shopping_cart),
             color: Theme.of(context).accentColor,
