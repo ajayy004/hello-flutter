979e33185a47edfb06238dbecf0efd62de7133ce
Ajay Yadav <ajayy101@gmail.com>
2020-07-08 18:51:29 -0400

    added form input handlers

--- flutter/shop_app/lib/screens/add_edit_product.dart
+++ flutter/shop_app/lib/screens/add_edit_product.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/providers/product.dart';
 
 class AddEditPrductScreen extends StatefulWidget {
   static const routeName = '/edit-product';
@@ -9,24 +10,52 @@ class AddEditPrductScreen extends StatefulWidget {
 class _AddEditPrductScreenState extends State<AddEditPrductScreen> {
   final _priceFocusNode = FocusNode();
   final _descriptionFocusNode = FocusNode();
+  final _imageUrlController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  var _editedProduct = Product(
+    id: null,
+    title: '',
+    price: 0,
+    description: '',
+    imageUrl: '',
+    isFavorite: false,
+  );
 
   // To avoid memory leak
   @override
   void dispose() {
     _priceFocusNode.dispose();
     _descriptionFocusNode.dispose();
+    _imageUrlController.dispose();
     super.dispose();
+  }
+
+  void _saveForms() {
+    _formKey.currentState.save();
+    print(_editedProduct.title);
+    print(_editedProduct.price);
+    print(_editedProduct.description);
+    print(_editedProduct.imageUrl);
   }
 
   @override
   Widget build(BuildContext context) {
+    print(_imageUrlController.text);
     return Scaffold(
       appBar: AppBar(
         title: Text('Edit Product'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.save),
+            onPressed: () => _saveForms(),
+          )
+        ],
       ),
       body: Padding(
         padding: EdgeInsets.all(16),
         child: Form(
+          key: _formKey,
           child: ListView(
             children: <Widget>[
               TextFormField(
@@ -34,6 +63,16 @@ class _AddEditPrductScreenState extends State<AddEditPrductScreen> {
                 textInputAction: TextInputAction.next,
                 onFieldSubmitted: (_) {
                   FocusScope.of(context).requestFocus(_priceFocusNode);
+                },
+                onSaved: (value) {
+                  _editedProduct = Product(
+                    title: value,
+                    price: _editedProduct.price,
+                    description: _editedProduct.description,
+                    id: _editedProduct.id,
+                    imageUrl: _editedProduct.imageUrl,
+                    isFavorite: _editedProduct.isFavorite,
+                  );
                 },
               ),
               TextFormField(
@@ -44,12 +83,51 @@ class _AddEditPrductScreenState extends State<AddEditPrductScreen> {
                 onFieldSubmitted: (_) {
                   FocusScope.of(context).requestFocus(_descriptionFocusNode);
                 },
+                onSaved: (value) {
+                  _editedProduct = Product(
+                    title: _editedProduct.title,
+                    price: double.parse(value),
+                    description: _editedProduct.description,
+                    id: _editedProduct.id,
+                    imageUrl: _editedProduct.imageUrl,
+                    isFavorite: _editedProduct.isFavorite,
+                  );
+                },
               ),
               TextFormField(
                 decoration: InputDecoration(labelText: 'Description'),
                 maxLines: 3,
                 keyboardType: TextInputType.multiline,
                 focusNode: _descriptionFocusNode,
+                onSaved: (value) {
+                  _editedProduct = Product(
+                    title: _editedProduct.title,
+                    price: _editedProduct.price,
+                    description: value,
+                    id: _editedProduct.id,
+                    imageUrl: _editedProduct.imageUrl,
+                    isFavorite: _editedProduct.isFavorite,
+                  );
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Image URL'),
+                keyboardType: TextInputType.url,
+                textInputAction: TextInputAction.done,
+                controller: _imageUrlController,
+                onSaved: (value) {
+                  _editedProduct = Product(
+                    title: _editedProduct.title,
+                    price: _editedProduct.price,
+                    description: _editedProduct.description,
+                    id: _editedProduct.id,
+                    imageUrl: value,
+                    isFavorite: _editedProduct.isFavorite,
+                  );
+                },
+                onFieldSubmitted: (_) {
+                  _saveForms();
+                },
               ),
             ],
           ),
